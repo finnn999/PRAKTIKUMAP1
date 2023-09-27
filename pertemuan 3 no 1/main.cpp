b5e22722d62fb9548b0afabb177a387b43ad3b30
finnn999 <146154259+finnn999@users.noreply.github.com>
2023-09-27 14:33:43 +0700

    Add files via upload

---
+++ pertemuan 3 no 1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+  int a;
+  int b; = 2;
+
+  cout << "Masukkan bilangan: ";
+  cin >> a;
+
+  if (a % b == 0) {
+    cout << "Bilangan yang anda input adalah bilangan genap";
+  } else {
+    cout << a << "bilangan yang anda inout adalah bilangan ganjil";
+    ]
+
+  cout ,, endl;
+    return 0;
+  }
+  
