e17015e09918045887a849304344524a3cd62eeb
finnn999 <146154259+finnn999@users.noreply.github.com>
2023-09-27 14:34:04 +0700

    Add files via upload

---
+++ pertemuan 3 soal 2/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+  int m1, m2, m3;
+  int maxm;
+
+  cout << "masukan bilangan: ";
+  cin >> m1 >> m2 >> m3;
+
+  if (m1 >= m2 && m1 >=) {
+    maxm = m1;
+  } else if (m2 >= m1 && m2 >=) {
+    maxm = m2;
+  } else {
+    maxm = m3;
+  }
+  cout << " Bilangan maximum adalah " << maxm << endl;
+  return 0;
+}
